ac3e68159a199ffdd8471fdf093968c2dabd107f
Will-Hellinger <will.hellinger@gmail.com>
2024-08-06 14:41:51 -0500

    more type hints + more accurate vallidation + army camp etc

--- src/main.py
+++ src/main.py
@@ -14,13 +14,14 @@ def validate_village(buildings: list[dict], building_cache: dict) -> bool:
     :return bool: True if the village is valid, False otherwise
     """
 
-    valid = True
+    valid: bool = True
     warnings: list[str] = []
 
     # Get the townhall building
-    townhall = None
-    previous_townhall = None
-    current_buildings = {}
+    townhall: dict | None = None
+    previous_townhall: dict | None = None
+    current_buildings: dict = {}
+    positions = []
 
     for building in buildings:
         building_type: str = building.get("type")
@@ -30,12 +31,18 @@ def validate_village(buildings: list[dict], building_cache: dict) -> bool:
             townhall = building
         
             if 'townhall' in building_cache and building_level > 1:
-                previous_townhall: dict = building_cache['townhall'][str(building_level - 1)]
+                previous_townhall = building_cache['townhall'][str(building_level - 1)]
             
         if building_type not in current_buildings:
             current_buildings[building_type] = [building_level]
         else:
             current_buildings[building_type].append(building_level)
+        
+        if not building.get('positions') in positions:
+            positions.append(building.get('positions'))
+        else:
+            warnings.append(f"Building {building_type} is overlapping with another building")
+            valid = False
     
     if townhall is None:
         print("No townhall found, unable to validate village!")
@@ -51,16 +58,15 @@ def validate_village(buildings: list[dict], building_cache: dict) -> bool:
     max_trap_level: dict = townhall.get('max level of traps')
 
     # Combine the dictionaries
-    max_combined_count = {}
+    max_combined_count: dict = {}
     for d in [max_resources_count, max_army_count, max_defense_count, max_trap_count]:
         for key, value in d.items():
             max_combined_count[key] = max_combined_count.get(key, 0) + value
     
-    max_combined_level = {}
+    max_combined_level: dict = {}
     for d in [max_resources_level, max_army_level, max_defense_level, max_trap_level]:
         for key, value in d.items():
             max_combined_level[key] = max_combined_level.get(key, 0) + value
-    
     
     for building_type in current_buildings:
         if building_type not in max_combined_count.keys() and building_type != 'townhall':
@@ -91,17 +97,16 @@ def validate_village(buildings: list[dict], building_cache: dict) -> bool:
             print(f'[!] {warning}')
         return valid
     
-    min_resources_count = previous_townhall.get('max number of resource')
-    min_army_count = previous_townhall.get('max number of army')
-    min_defense_count = previous_townhall.get('max number of defense')
-    min_trap_count = previous_townhall.get('max number of traps')
+    min_resources_count: dict = previous_townhall.get('max number of resource')
+    min_army_count: dict = previous_townhall.get('max number of army')
+    min_defense_count: dict = previous_townhall.get('max number of defense')
+    min_trap_count: dict = previous_townhall.get('max number of traps')
 
     # Combine the dictionaries
-    min_combined_count = {}
+    min_combined_count: dict = {}
     for d in [min_resources_count, min_army_count, min_defense_count, min_trap_count]:
         for key, value in d.items():
             min_combined_count[key] = min_combined_count.get(key, 0) + value
-
 
     for min_building in min_combined_count.keys():
         if min_building not in current_buildings:
@@ -128,7 +133,7 @@ def generate_sctructure_list(buildings: list[dict], building_cache: dict) -> lis
     :return list[dict]: The list of structures
     """
 
-    structures = []
+    structures: list[dict] = []
 
     for building in buildings:
         building_type: str = building.get("type")
@@ -139,6 +144,7 @@ def generate_sctructure_list(buildings: list[dict], building_cache: dict) -> lis
             building_cache[building_type] = building_data
 
         building_size: dict = building_cache[building_type].get("size")
+        building_inset: float = building_cache[building_type].get("inset")
 
         if building_cache[building_type].get("max count") == 1:
             structure: dict = building_cache[building_type][building_level]
@@ -146,24 +152,30 @@ def generate_sctructure_list(buildings: list[dict], building_cache: dict) -> lis
             structure['type'] = building_type
             structure['position'] = [building.get("x"), building.get("y")]
             structure['size'] = building_size
+            structure['inset'] = building_inset
 
             structures.append(structure)
             
         else:
             building_position: list[list[int]] = building.get('positions')
+
             for position in building_position:
                 structure: dict = copy.deepcopy(building_cache[building_type][building_level])
                 structure['level'] = building_level
                 structure['type'] = building_type
                 structure['position'] = position
                 structure['size'] = building_size
+                structure['inset'] = building_inset
+                
+                if building_type == 'army camp':
+                    structure['troops'] = building.get('troops')
 
                 structures.append(structure)
 
     return structures
 
 
-def main(cell_size: int, num_rows: int, num_cols: int, village: dict, building_colors: dict) -> None:
+def main(cell_size: int, num_rows: int, num_cols: int, fps: int, village: dict, building_colors: dict) -> None:
     """
     The main function of the program
 
@@ -174,18 +186,20 @@ def main(cell_size: int, num_rows: int, num_cols: int, village: dict, building_c
     :param building_colors: The colors of the buildings
     :return None:
     """
+    
+    if village is None:
+        print("No village data found")
+        return None
+
     pygame.init()
 
-    print(cell_size)
-    window_width = num_cols * cell_size
-    window_height = num_rows * cell_size
+    window_width: int = num_cols * cell_size
+    window_height: int = num_rows * cell_size
 
-    window = pygame.display.set_mode((window_width, window_height))
+    window: pygame.surface = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption("Grid Window")
 
-
-    if village is None:
-        return
+    clock: pygame.time.Clock = pygame.time.Clock()
     
     buildings: list[dict] | list = village.get("buildings", [])
     building_cache: dict = {}
@@ -214,12 +228,12 @@ def main(cell_size: int, num_rows: int, num_cols: int, village: dict, building_c
         
         # Draw the structures
         for structure in structures:
-            position = structure.get("position")
-            size = structure.get("size")
+            position: list[int] = structure.get("position")
+            size: dict = structure.get("size")
 
             # Calculate the center position of the structure
-            center_x = (position[0] + size.get('width') // 2) * cell_size
-            center_y = (position[1] + size.get('height') // 2) * cell_size
+            center_x: int = (position[0] + size.get('width') // 2) * cell_size
+            center_y: int = (position[1] + size.get('height') // 2) * cell_size
 
             # Adjust center position for odd-sized structures
             if size.get('width') % 2 != 0:
@@ -230,8 +244,8 @@ def main(cell_size: int, num_rows: int, num_cols: int, village: dict, building_c
             # Draw the structure
             for row in range(size.get('width')):
                 for col in range(size.get('height')):
-                    x = (position[0] + col) * cell_size
-                    y = (position[1] + row) * cell_size
+                    x: int = (position[0] + col) * cell_size
+                    y: int = (position[1] + row) * cell_size
 
                     # Use building color
                     if structure.get('type') in building_colors:
@@ -240,14 +254,17 @@ def main(cell_size: int, num_rows: int, num_cols: int, village: dict, building_c
                         pygame.draw.rect(window, (0, 0, 0), (x, y, cell_size, cell_size))
 
             # Draw level in the middle
-            level = structure.get('level')
-            font = pygame.font.Font(None, 20)
+            level: int = structure.get('level')
+            font: pygame.font = pygame.font.Font(None, 20)
             text = font.render(str(level), True, (0, 0, 0))
             text_rect = text.get_rect(center=(center_x, center_y))
             window.blit(text, text_rect)
                     
         # Update the display
         pygame.display.flip()
+
+        # Limit the frame rate
+        clock.tick(fps)
 
 
 if __name__ == "__main__":
@@ -260,7 +277,7 @@ if __name__ == "__main__":
     num_rows: int = config.get("rows", 44)
     num_cols: int = config.get("cols", 44)
     building_colors: dict = config.get("building_colors", {})
-    village = None
+    village: dict | None = None
 
     # Parse the command line arguments
     parser = argparse.ArgumentParser()
@@ -268,6 +285,7 @@ if __name__ == "__main__":
     parser.add_argument("--cell_size", type=int, default=cell_size)
     parser.add_argument("--num_rows", type=int, default=num_rows)
     parser.add_argument("--num_cols", type=int, default=num_cols)
+    parser.add_argument("--fps", type=int, default=60)
     parser.add_argument("--input_village", type=str, default=None)
 
     args = parser.parse_args()
@@ -288,5 +306,5 @@ if __name__ == "__main__":
             village: dict = json.load(file)
 
     # Run the main function
-    main(cell_size, num_rows, num_cols, village, building_colors)
+    main(cell_size, num_rows, num_cols, args.fps, village, building_colors)
     pygame.quit()
